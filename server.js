47d31132991406b1b525bb63e190117792f8a16e
RM TENS <tens.rm@gmail.com>
2013-01-15 12:08:11 +0900

    server.jsを修正しました

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
-http.createServer( function( req, res ) {
-						res.writeHead( 200, {'Content-Type':'text/plain'});
-						res.end('Hello NodeNinja');
-					}
-				 ).listen( 8080 );
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello NodeNinjan');
+}).listen(8080);
