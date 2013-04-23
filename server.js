538a6bb08b57d27aef134cdad54ba006ccd8feef
RM TENS <tens.rm@gmail.com>
2013-04-23 14:43:30 +0900

    POST処理のテスト

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+var sys  = require('sys')
 var http = require('http');
+var url  = require('url');
+var query= require('querystring');
 
 http.createServer(function(req,res){
 
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Hello World 5");
-	res.end();
+	if( req.method=='POST' )
+	{
+		var body = '';
+		req.on( 'data', function(data){ body += data; } );
+		req.on( 'end', function() {
+				var POST  = query.parse( body );
+				res.writeHead(200, {"Content-Type": "text/plain"});
+				res.write(POST);
+				res.end();
+		} );
+	}
+	else
+	if( req.method=='GET' )
+	{
+	}
 
 }).listen(8000);
